007d3c9f346f58dea30a78667b11c54655cac066
Karen <k-celis@javeriana.edu.co>
2019-08-10 16:58:38 -0500

    Punto 4

--- fibonacci/app/src/main/java/com/example/fibonaci/MainActivity.java
+++ fibonacci/app/src/main/java/com/example/fibonaci/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     int num1;
     int num2;
     TextView texto;
+    LinearLayout linear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         num2 = 1;
         boton2 = findViewById(R.id.calcular1);
         texto = findViewById(R.id.textView2);
+        linear=findViewById(R.id.linear2);
         boton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,7 +39,12 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("tes", aux);
                 num1 = num2;
                 num2 = num3;
-                texto.setText(aux);
+                TextView textView = new TextView(view.getContext());
+                textView.setText(aux2);
+                textView.setId(num3);
+                linear.addView(textView);
+
+              //  texto.setText(aux);
             }
         });
     }
