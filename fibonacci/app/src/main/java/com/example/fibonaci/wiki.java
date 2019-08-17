66d03240372ab2a583bd4206fea4b74ea359d7df
Karen C <k-celis@javeriana.edu.co>
2019-08-17 18:18:02 -0500

    Create botton with image

---
+++ fibonacci/app/src/main/java/com/example/fibonaci/wiki.java
@@ -0,0 +1,14 @@
+package com.example.fibonaci;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class wiki extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wiki);
+    }
+}
